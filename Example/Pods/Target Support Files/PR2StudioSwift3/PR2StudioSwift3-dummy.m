ffa683de90f623d8218f097ab2fe86a31b26ee95
Pablo Roca <pablo@kabbee.com>
2017-03-23 15:24:52 +0100

    progress

--- Example/Pods/Target Support Files/PR2StudioSwift3/PR2StudioSwift3-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_PR2StudioSwift3 : NSObject
-@end
-@implementation PodsDummy_PR2StudioSwift3
-@end
--- Example/Pods/Target Support Files/Pods-PR2StudioSwift3_Tests/Pods-PR2StudioSwift3_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PR2StudioSwift3_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_PR2StudioSwift3_Tests
-@end
